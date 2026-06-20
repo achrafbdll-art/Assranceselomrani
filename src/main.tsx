fad11dfa5e2d5cb06676f9c825d8eabcd975a740
achrafbdll-art <achrafbdll@gmail.com>
2026-06-20 21:25:31 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import logo from './ae-logo.svg';
+<img src={logo} alt="Assurances ELOMRANI" ... />
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
