6f37872fac6b5afc1c24dfb97d4867e20c4720dd
achrafbdll-art <achrafbdll@gmail.com>
2026-06-20 23:31:46 +0200

    Refactor main.tsx to define App component
    Refactored main.tsx to define and export the App component, which displays a logo.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
 import logo from './ae-logo.svg';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div>
+      <img src={logo} alt="Assurances ELOMRANI" />
+    </div>
+  );
+}
+
+export default App;
+
