a14eef41341a76182903ebfa3c20b8e65cac3502
achrafbdll-art <achrafbdll@gmail.com>
2026-06-20 20:53:56 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,7 @@ const App: React.FC = () => {
                 className="absolute inset-0"
               >
                 <img 
-                  src={slides[currentSlide].image} 
+                  src={ae-logo.svg].image} 
                   alt="Assurances ELOMRANI" 
                   className="w-full h-full object-cover"
                   referrerPolicy="no-referrer"
